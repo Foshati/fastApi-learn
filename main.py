90f946063ec0271777c08a79c482b7d707ff27b1
Foshati <amirrezafoshati@gmail.com>
2024-07-13 18:16:06 +0330

    run with uvicorn

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 
+# import uvicorn
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "hello world"}
+
+
+# if __name__ == "__main__":
+#     uvicorn.run(app)
