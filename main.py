c3c7b781315008ca94d677a0dc79c3f2979934aa
foshati <amirrezafoshati@gmail.com>
2025-05-16 02:48:21 +0330

    Remove pyproject.toml and delete unused router files; update requirements.txt with new package versions and remove Python version constraints.

--- main.py
+++ main.py
@@ -1,43 +1,9 @@
-# from typing import Annotated
-# from fastapi import Body, Depends, FastAPI
-# from pydantic import BaseModel
+from fastapi import FastAPI
 
 
-# app = FastAPI()
+app = FastAPI()
 
 
-# class createPostIn(BaseModel):
-#     title: str
-#     content: str
-#     rate: int
-
-
-# @app.get("/")
-# async def root():
-#     return {"message": "hello world"}
-
-
-# @app.get("/blog/{slug}")
-# async def blog(slug: int):
-#     return {"message": slug}
-
-
-# @app.get("/blog/{slug}")
-# async def blogPrivate(slug: int, is_private: bool = False):
-#     return {"message": slug}
-
-
-# @app.post("/blog/create")
-# async def blogCreate(post: createPostIn):
-#     return post
-
-
-# # Dependency Injection in fastAPI
-# # q: Union[str, None] = None
-# async def help_params(q: str | None = None, limit: int = 20):
-#     return {"query": q, "limitQuery": limit}
-
-
-# @app.get("/items")
-# async def reade_items(allItems: Annotated[dict, Depends(help_params)]):
-#     return allItems
+@app.get("/")
+def root():
+    return {"message": "hello world"}
