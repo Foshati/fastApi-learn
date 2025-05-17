aa539ff21fe334dafa29c1f42b1fc4d12ba8146b
foshati <amirrezafoshati@gmail.com>
2025-05-17 23:55:56 +0330

    create api path parameters

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "hello world"}
+
+
+
+@app.get("/home/{name}/{age}")
+def info(name, age):
+    return {"message" : f "{name} is {age} old" }
